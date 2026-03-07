d5ea5d425173acddd741deae849a51d7a4622d10
Vaalz <revalfahrizal87@gmail.com>
2026-03-07 14:14:07 +0700

    update berita

---
+++ backend/config/image.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Image Driver
+    |--------------------------------------------------------------------------
+    |
+    | Intervention Image supports "GD Library" and "Imagick" to process images
+    | internally. You may choose one of them according to your PHP
+    | configuration. By default PHP's "GD Library" implementation is used.
+    |
+    | Supported: Intervention\Image\Drivers\Gd\Driver::class, Intervention\Image\Drivers\Imagick\Driver::class
+    |
+    */
+
+    'driver' => Intervention\Image\Drivers\Gd\Driver::class,
+];
